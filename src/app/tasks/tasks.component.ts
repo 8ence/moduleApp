35df4f479442992a2beaedba04acf3e81fbf5cbe
Kővári Bence <kovaribence95@gmail.com>
2025-01-24 10:15:19 +0100

    Refactor task management: integrate TasksService for adding and retrieving tasks, update AddTaskComponent to use userId input, and modify event emissions for task creation and cancellation.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,70 +1,34 @@
 import { type NewTask } from './task/task.model';
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "./task/task.component";
-import { AddTaskComponent } from "./add-task/add-task.component";
-import { CardComponent } from "../shared/card/card.component";
-
+import { TaskComponent } from './task/task.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { CardComponent } from '../shared/card/card.component';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   imports: [TaskComponent, AddTaskComponent, CardComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss'
+  styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
-
-  @Input({required: true}) userId!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
   newTaskIsVisible = false;
 
-  tasks = [
-    {
-      id: "1",
-      userId: "u1",
-      dueDate: '2021-12-31',
-      name: 'Lakcmíkeresés hiba javítása',
-      description: 'JIRA 4410 - Lakcímkeresési hiba javítása',
-    },
-    {
-      id: "2",
-      userId: "u2",
-      dueDate: '2021-12-31',
-      name: 'Örököskeresés hiba javítása',
-      description: 'JIRA 4411 - Örököskeresési hiba javítása',
-    },
-    {
-      id: "3",
-      userId: "u5",
-      dueDate: '2021-12-31',
-      name: 'Folyamatdefiníció hiba javítása',
-      description: 'JIRA 4412 - Folyamatdefiníció hiba javítása',
-    }
-  ];
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter(task => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
-  completeTask = (id: string) => {
-    this.tasks = this.tasks.filter(task => task.id !== id);
-  }
+  completeTask = (taskId: string) => {};
 
   createNewTask = () => {
     this.newTaskIsVisible = true;
-  }
+  };
 
-  cancelTaskCreation = () => {
+  closeAddTask = () => {
     this.newTaskIsVisible = false;
-  }
-
-  addTask = (taskData: NewTask) => {
-    this.tasks.unshift({
-      id: Math.random().toString(),
-      userId: this.userId,
-      dueDate: taskData.date,
-      name: taskData.name,
-      description: taskData.description,
-    });
-    this.newTaskIsVisible = false;
-  }
+  };
 }
